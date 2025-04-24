cbd938c3b8d83251cd2a8842796d8aa25e77f8ac
Dirk Weber <dwr@dasburo.com>
2025-04-24 11:40:17 +0200

    fix: simplify persistedQuery construction and improve fetch URL

--- netlify/functions/pageBySlugAndParentPath.js
+++ netlify/functions/pageBySlugAndParentPath.js
@@ -2,8 +2,9 @@ export async function handler(event, _context) {
     const slug = event.queryStringParameters.slug ?? "";
     const parentPath = event.queryStringParameters.parentPath ?? "/";
     const PUBLISHER_HOST = 'https://dev.cf.skoda-auto.com'
-    const persistedQuery = `test-site/pageBySlugAndParentPath%2BparentPath%3D%2Fcontent%2Fdam%2Fskoda-auto-dirk%2F${parentPath}%3Bslug%3D%2Fimprint;`
-    const res = await fetch(`${PUBLISHER_HOST}/graphql/execute.json/${persistedQuery}%2BparentPath%3D%2Fcontent%2Fdam%2Fskoda-auto-dirk%2F%2F%3Bslug%3D%2F${slug};`, {});
+    const persistedQuery = `test-site/pageBySlugAndParentPath`
+    const url = `${PUBLISHER_HOST}/graphql/execute.json/${persistedQuery}%2BparentPath%3D%2Fcontent%2Fdam%2Fskoda-auto-dirk%2F${parentPath}%3Bslug%3D%2F${slug};`
+    const res = await fetch(url)
 
     if (!res.ok) {
         return {

a2b42f70e8156e9eef45a26672108101c4d8c6d6
Dirk Weber <dwr@dasburo.com>
2025-04-24 11:44:39 +0200

    fix: correct URL construction in pageBySlugAndParentPath handler

--- netlify/functions/pageBySlugAndParentPath.js
+++ netlify/functions/pageBySlugAndParentPath.js
@@ -3,7 +3,7 @@ export async function handler(event, _context) {
     const parentPath = event.queryStringParameters.parentPath ?? "/";
     const PUBLISHER_HOST = 'https://dev.cf.skoda-auto.com'
     const persistedQuery = `test-site/pageBySlugAndParentPath`
-    const url = `${PUBLISHER_HOST}/graphql/execute.json/${persistedQuery}%2BparentPath%3D%2Fcontent%2Fdam%2Fskoda-auto-dirk%2F${parentPath}%3Bslug%3D%2F${slug};`
+    const url = `${PUBLISHER_HOST}/graphql/execute.json/${persistedQuery}%3BparentPath%3D%2Fcontent%2Fdam%2Fskoda-auto-dirk%2F${parentPath}%3Bslug%3D${slug};`
     const res = await fetch(url)
 
     if (!res.ok) {
